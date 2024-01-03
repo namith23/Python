3a0a4dbeee0fbe8a17ebaf70a307d77a147d16b9
Namith <142716060+namith23@users.noreply.github.com>
2024-01-03 22:10:04 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,15 @@
+name1 = input("What is your name? ")
+print(name1)
+name2 = input("What is your name? ")
+print(name2)
+name3=input("What is your name? ")
+print(name3)
+
+
+num1 = int(input("What is the first number: "))
+num2 = int(input("What is the second number: "))
+print(num1 * num2)#use int when numbers are used
+
+#Write a program that calculates and outputs the number of characters in any name. The automated tests will try out lots of different names as the input. Your code should work for any name.
+name=input()
+print(len(name))
