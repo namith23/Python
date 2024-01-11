e57becea1c2c572f730bfe34c0f0f71b19f91eee
Namith <142716060+namith23@users.noreply.github.com>
2024-01-11 22:07:27 +0530

    Add files via upload

---
+++ bmi2.py
@@ -0,0 +1,14 @@
+height = float(input())
+weight = int(input())
+
+bmi = weight / (height * height)
+if bmi < 18.5:
+  print(f'your BMI is {bmi} you are underweight')
+elif bmi < 25:
+  print(f'your BMI is {bmi} you are idealweight')
+elif bmi < 30:
+  print(f'your BMI is {bmi} you are overweight')
+elif bmi < 35:
+  print(f'your BMI is {bmi} you are obese')
+else:
+  print(f'your BMI is {bmi} you are critical')
