242d1c736a797b0d621517882e09295ff0b803f1
Namith <142716060+namith23@users.noreply.github.com>
2024-01-17 21:14:52 +0530

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,7 @@
+names_string = "Alice, Bob, Charlie, David, Emily"
+names = names_string.split(", ")
+import random
+num_items=len(names)
+random_choice = random.randint(0, num_items -1)
+person_who_will_pay = names[random_choice]
+print (person_who_will_pay + " is going to buy the meal today!")
