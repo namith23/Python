c7d33918cbf6e443bb7967257a7e38a3607e4115
Namith <142716060+namith23@users.noreply.github.com>
2024-01-22 21:53:17 +0530

    Add files via upload

---
+++ ex8.py
@@ -0,0 +1,10 @@
+target = 100
+for num in range(1, target+1):
+    if num % 3 == 0 and num % 5 == 0:
+        print('FizzBuzz')
+    elif num % 3 == 0:
+        print('Fizz')
+    elif num % 5 == 0:
+        print('Buzz')
+    else:
+        print(num)
