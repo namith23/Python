703d4b0b2758c6d60c316d80a4b0e0105c76a263
Namith <142716060+namith23@users.noreply.github.com>
2024-02-07 22:11:55 +0530

    Add files via upload

---
+++ ex20.py
@@ -0,0 +1,9 @@
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+bmi = weight/height ** 2
+
+if height > 3:
+    raise ValueError("The height must not be more than 3")
+
+print(bmi)
