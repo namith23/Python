c09fb7a4b192149ece0a500c1302b51f071f88d8
Namith <142716060+namith23@users.noreply.github.com>
2024-02-18 22:10:38 +0530

    Update proj5.py

--- proj5.py
+++ proj5.py
@@ -1,3 +1,4 @@
+#password generator 
 import random
 letters = ['a', 'b','d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm' '0' 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', '0', 'P', 'Q', 'R', 'S', 'T', 'U', 'V' 'W', 'X', 'Y', 'Z'] 
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
