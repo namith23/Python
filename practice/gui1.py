e6e162d24d0953619577ee6d0b0db8b1f99606dd
Namith <142716060+namith23@users.noreply.github.com>
2024-03-03 22:07:35 +0530

    Add files via upload

---
+++ practice/gui1.py
@@ -0,0 +1,20 @@
+import tkinter
+window = tkinter.Tk()
+window.title("My 1st GUI ")
+window.minsize(width=500, height=300)
+
+label = tkinter.Label(text="I am a Label", font="arial", 24)
+label.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
